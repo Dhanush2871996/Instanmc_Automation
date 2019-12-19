5e9a0a9c64ce6cb39b345f0e6f759b7e03a449c0
Dhanush287 <rakesh2871996@gmail.com>
2019-12-19 09:52:27 +0530

    Frame work 004

--- src/test/java/com/nmc/utilities/Reporting.java
+++ src/test/java/com/nmc/utilities/Reporting.java
@@ -1,4 +1,5 @@
 package com.nmc.utilities;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -14,26 +15,25 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+
 /**
  * This is an listener class used to generate the Extent Reports
  * 
- * */
-public class Reporting extends TestListenerAdapter
-{
+ */
+public class Reporting extends TestListenerAdapter {
 	public ExtentHtmlReporter htmlReporter;
 	public ExtentReports extent;
 	public ExtentTest logger;
-	
-	public void onStart(ITestContext testContext)
-	{
-		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());//current time stamp
-		String repName = "Test-Report-"+timeStamp+".html";
-		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")+"/test/output/"+repName);
-		//htmlReporter.loadXMLConfig(System.getProperty("user.dir"+"/extent-config.xml"));
-		htmlReporter.loadXMLConfig(System.getProperty("user.dir")+"/extent-config.xml");
-		
+
+	public void onStart(ITestContext testContext) {
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// current time stamp
+		String repName = "Test-Report-" + timeStamp + ".html";
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test/output/" + repName);
+		// htmlReporter.loadXMLConfig(System.getProperty("user.dir"+"/extent-config.xml"));
+		htmlReporter.loadXMLConfig(System.getProperty("user.dir") + "/extent-config.xml");
+
 		extent = new ExtentReports();
-		
+
 		extent.attachReporter(htmlReporter);
 		extent.setSystemInfo("Host Name", "localhost");
 		extent.setSystemInfo("Enviroment", "QA");
@@ -41,48 +41,41 @@ public class Reporting extends TestListenerAdapter
 
 		htmlReporter.config().setDocumentTitle("Instanmc Automation");
 		htmlReporter.config().setReportName("Functional Test Automation Report");
-		//htmlReporter.config().setTestViewChartLocation(Chart);
+		// htmlReporter.config().setTestViewChartLocation(Chart);
 		htmlReporter.config().setTheme(Theme.DARK);
-		
+
 	}
 
-	public void onTestSuccess(ITestResult testResult)
-	{
+	public void onTestSuccess(ITestResult testResult) {
 		logger = extent.createTest(testResult.getName());
 		logger.log(Status.PASS, MarkupHelper.createLabel(testResult.getName(), ExtentColor.GREEN));
 	}
-	
-	public void onTestFailure(ITestResult testResult)
-	{
+
+	public void onTestFailure(ITestResult testResult) {
 		logger = extent.createTest(testResult.getName());
 		logger.log(Status.FAIL, MarkupHelper.createLabel(testResult.getName(), ExtentColor.RED));
-		
-		//String screenShotPath = System.getProperty("user.dir")+"/ScreenShots/"+testResult.getName()+".png";
-		String screenShotPath = "home\\manish\\Desktop\\PracticeCodes\\SeleniumHomeProjects\\ProjName-INetBankingVersion1\\ScreenShots"+testResult.getName()+".png";
-		
+
+		// String screenShotPath =
+		// System.getProperty("user.dir")+"/ScreenShots/"+testResult.getName()+".png";
+		String screenShotPath = System.getProperty("user.dir")+"/ScreenShots"+ testResult.getName() + ".png";
+
 		File file = new File(screenShotPath);
-		
-		if(file.exists())
-		{
-			try 
-			{
-				logger.fail("Screenshot is below : "+logger.addScreenCaptureFromPath(screenShotPath));
-			}
-			catch(IOException e)
-			{
+
+		if (file.exists()) {
+			try {
+				logger.fail("Screenshot is below : " + logger.addScreenCaptureFromPath(screenShotPath));
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	public void onTestSkipped(ITestResult testResult)
-	{
+
+	public void onTestSkipped(ITestResult testResult) {
 		logger = extent.createTest(testResult.getName());
 		logger.log(Status.SKIP, MarkupHelper.createLabel(testResult.getName(), ExtentColor.ORANGE));
 	}
-	
-	public void onFinish(ITestContext testContext)
-	{
+
+	public void onFinish(ITestContext testContext) {
 		extent.flush();
 	}
 }
